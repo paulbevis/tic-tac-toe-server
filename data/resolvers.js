1d1cb0cb023ce8aeae18d432f93b8b1ebb275cbc
Paul Bevis <paulbevis2010@gmail.com>
2016-11-15 22:06:13 +1000

    Join or create functionality added now

--- data/resolvers.js
+++ data/resolvers.js
@@ -10,6 +10,27 @@ const winningPatterns = [
 
 const games = [];
 
+
+const createNewGame = function() {
+  const game = {
+    id: games.length,
+    status: 'Waiting',
+    players: [],
+    cells: [
+      {id: 0},
+      {id: 1},
+      {id: 2},
+      {id: 3},
+      {id: 4},
+      {id: 5},
+      {id: 6},
+      {id: 7},
+      {id: 8}
+    ]
+  };
+  return game;
+};
+
 const checkIfGameOver = (game) => {
   const valueNotSet = cell => !propEq('value', 0)
   if (all(valueNotSet)(game.cells)) {
@@ -37,6 +58,7 @@ const resolveFunctions = {
       return games[gameBoardId];
     },
     firstAvailableGameBoard(){
+      console.log('firstAvailableGameBoard called: result: ', games);
       return find(propEq('status', 'Waiting'))(games)
     },
     currentCounterResult(){
@@ -50,29 +72,29 @@ const resolveFunctions = {
       pubsub.publish('counterChanged', counter);
       return counter
     },
-    registerPlayer(_, {playerId, gameBoardId}) {
+    registerPlayer(_, {playerId, gameBoardId, playerName}) {
       const game = games[gameBoardId];
       if (game.players.length === 2) {
         game.players = [];
       }
       if (game.players.length === 1 && game.players[0].id !== playerId) {
-        game.players.push({id: playerId, value: 'O'})
+        game.players.push({id: playerId, value: 'O', name: playerName})
         if (game.players.length === 2) {
           game.status = 'Playing';
         } else {
           game.status = 'Waiting';
         }
-        pubsub.publish('gameUpdated', game);
+        // pubsub.publish('gameUpdated', game);
       }
       if (game.players.length === 0) {
-        game.players.push({id: playerId, value: 'X'});
+        game.players.push({id: playerId, value: 'X', name: playerName});
         const resetCellValue = (cell)=> {
           cell.value = '';
           return cell
         };
         game.cells = map(resetCellValue, game.cells);
 
-        pubsub.publish('gameUpdated', game);
+        // pubsub.publish('gameUpdated', game);
       }
       return game.players[game.players.length - 1];
     },
@@ -88,25 +110,25 @@ const resolveFunctions = {
       return game.cells[cellId];
     },
 
-    createNewGame(_, {}){
-      const game = {
-        id: games.length,
-        status: 'Waiting',
-        players: [],
-        cells: [
-          {id: 0},
-          {id: 1},
-          {id: 2},
-          {id: 3},
-          {id: 4},
-          {id: 5},
-          {id: 6},
-          {id: 7},
-          {id: 8}
-        ]
-      };
-      games.push(game);
-      pubsub.publish('newGameCreated', game);
+    joinGame(_, {playerId, playerName}) {
+      const player = {id: playerId, name: playerName}
+      const onlyOnePlayerThatIsNotYou = (game)=>game.players.length === 1 && game.players[0].id !== playerId;
+      let game = find(onlyOnePlayerThatIsNotYou)(games);
+      if (!game) {
+        console.log('Creating new game...')
+        game = createNewGame();
+        games.push(game);
+      } else {
+        console.log('game exists: ')
+      }
+      if (game.players.length === 0) {
+        player.value = 'X';
+      } else {
+        player.value = 'O';
+      }
+      game.players.push(player);
+      pubsub.publish('gameJoined', game);
+
       return game.id;
     }
 
@@ -116,14 +138,21 @@ const resolveFunctions = {
     gameUpdated(game) {
       return game;
     },
-    cellSelected(game) {
+    cellSelected(game)
+    {
       return game;
     },
-    newGameCreated(game) {
+    newGameCreated(game)
+    {
       return game;
     },
-    counterChanged(counter){
+    counterChanged(counter)
+    {
       return counter;
+    },
+    gameJoined(game)
+    {
+      return game;
     }
   }
 };
